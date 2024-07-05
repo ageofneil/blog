85ef6fc9b4166099e4abba5b81cccefbb342289f
Neil Turner <neil@fortnine.ca>
2024-07-05 12:54:44 -0400

    my first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "AgeOfNeil",
+  url: "https://ageofneil.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Neil turner",
+  links: {
+    twitter: "https://twitter.com/ageofneil",
+    github: "https://github.com/ageofneil",
+    personalSite: "https://ageofneil.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
