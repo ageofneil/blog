02d87f07f0d6009e596e4a88e3458e570114830e
Neil Turner <neil@fortnine.ca>
2024-07-05 21:08:01 -0400

    remove blog from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { buttonVariants } from "@/components/ui/button";
 import { siteConfig } from "@/config/site";
 import { cn, sortPosts } from "@/lib/utils";
-import { posts } from "#site/content";
 import Link from "next/link";
-import { PostItem } from "@/components/post-item";
 
 export default function Home() {
-  const latestPosts = sortPosts(posts).slice(0, 5);
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col gap-4 text-center">
+        <div className="flex justify-center items-center h-48 w-48 mx-auto">
+  <Avatar className="h-full w-full">
+    <AvatarImage src="/avatar.png" alt={siteConfig.author} />
+    <AvatarFallback>Neil Turner</AvatarFallback>
+  </Avatar>
+</div>
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
             Hello, I&apos;m Neil
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-          I help companies solve marketing problems using data.
+          I solve marketing problems using data.
           </p>
           <div className="flex flex-col gap-4 justify-center sm:flex-row">
             <Link
@@ -37,26 +41,6 @@ export default function Home() {
             </Link>
           </div>
         </div>
-      </section>
-      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
-        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
-          Latest Posts
-        </h2>
-        <ul className="flex flex-col">
-          {latestPosts.map((post) => (
-            post.published && (
-              <li key={post.slug} className="first:border-t first:border-border">
-                <PostItem
-                  slug={post.slug}
-                  title={post.title}
-                  description={post.description}
-                  date={post.date}
-                  tags={post.tags}
-                />
-              </li>
-            )
-          ))}
-        </ul>
       </section>
     </>
   );
