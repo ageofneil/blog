e9044d372e3c27d393ea67767db2f79d09d05521
Neil Turner <neil@fortnine.ca>
2025-07-09 10:06:45 -0400

    add new home page

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,14 @@ export default function Home() {
     <AvatarFallback>Neil Turner</AvatarFallback>
   </Avatar>
 </div>
-          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello, I&apos;m Neil
+          <h1 className="text-3xl sm:text-3xl md:text-3xl lg:text-3xl font-black text-balance">
+            The People Running Your Marketing Campaigns Shouldn’t Be Grading Their Own Work
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
-          I solve marketing problems using data.
+          Hi, I’m Neil. When marketing owns both execution and reporting, it’s easy for things to get blurry. I bring in third-party measurement to help teams see what’s actually driving results.
+          </p>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+          Let’s figure out the right way to measure your marketing. Whether that’s Causal Impact, Media Mix Modeling, Incrementality Testing, or something else entirely.
           </p>
           <div className="flex flex-col gap-4 justify-center sm:flex-row">
             <Link
